e64372928c3010dd659b937b58080550585b3ad7
saikrishna <saikrishnaarevarapu@gmail.com>
2023-11-10 16:14:24 +0530

    fourth problem is solved

---
+++ problem4.js
@@ -0,0 +1,7 @@
+export function problem4(inev){
+    let ans=[];
+    for(let i=0;i<inev.length;i++){
+        ans.push(inev[i]["car_year"]);
+    }
+    return ans;
+}
---
+++ test/testproblem4.js
@@ -0,0 +1,4 @@
+import { problem4 } from "../problem4.js";
+import { inventory } from "../inv.js";
+const years=problem4(inventory);
+console.log(years);
